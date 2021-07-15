f5db83a1ccdbae175a110ba24cc4510e2d1a7908
dxidegaming <31665473+dxidegaming@users.noreply.github.com>
2021-07-14 21:10:50 -0400

    with GridView
    also signed so it is runnable to a real device (iOS)

---
+++ Flix/MovieGridCell.swift
@@ -0,0 +1,15 @@
+//
+//  MovieGridCell.swift
+//  Flix
+//
+//  Created by Edison Enerio on 7/14/21.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    
+    @IBOutlet weak var posterView: UIImageView!
+    
+    
+}
